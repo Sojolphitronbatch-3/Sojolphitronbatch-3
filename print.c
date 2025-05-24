3359198aea1767fc1d987a0db43429640d6fbc84
Sojolphitronbatch-3 <130095619+Sojolphitronbatch-3@users.noreply.github.com>
2025-05-24 20:38:30 +0600

    Add files via upload

---
+++ print.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    printf("hellow world\n");
+    printf("hi");
+    //\n new line
+    // \t-4ta space
+    ///t t\t\t\t
+    return 0;
+}
---
+++ variable.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+
+{
+    int rahim=10;
+    float karim=20.3;
+    char a='z';
+    printf("%d\n %0.3f\n %c\n",rahim ,karim,a);
+
+    printf("Hello World");
+
+    return 0;
+}
